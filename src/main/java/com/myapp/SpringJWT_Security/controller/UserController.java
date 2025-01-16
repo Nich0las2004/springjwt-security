fde2e7a7ea760e0d21cee475894e64672d4376de
Nika Oniani <nikaoniani2018@gmail.com>
2025-01-16 21:15:56 +0400

    Replaced secret to be more secure, /api/login generates JWT successfully upon authenticated user

--- src/main/java/com/myapp/SpringJWT_Security/controller/UserController.java
+++ src/main/java/com/myapp/SpringJWT_Security/controller/UserController.java
@@ -2,27 +2,50 @@ package com.myapp.SpringJWT_Security.controller;
 
 import com.myapp.SpringJWT_Security.entity.User;
 import com.myapp.SpringJWT_Security.repository.UserRepository;
+import com.myapp.SpringJWT_Security.security.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UserController {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Autowired
-    public UserController(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+    public UserController(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder, JwtTokenProvider jwtTokenProvider) {
         this.userRepository = userRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     @PostMapping("/api/login")
     public String login() {
-        return "Welcome to the login page!";
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+            String username = authentication.getName();
+            List<String> roles = authentication.getAuthorities()
+                    .stream()
+                    .map(grantedAuthority -> grantedAuthority.getAuthority())
+                    .toList();
+
+            String jwtToken = jwtTokenProvider.generateToken(username, roles);
+
+            System.out.println(username);
+            System.out.println(roles);
+
+            System.out.println("JWT Token: " + jwtToken);
+
+            return "Welcome to the login page!";
+
     }
 
     @PostMapping("/api/register")
